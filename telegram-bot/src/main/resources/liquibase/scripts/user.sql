c852b7c9fcd46e4a0da50ef5546f92690e1c7197
Elena <lenulka.90@mail.ru>
2024-01-07 17:54:15 +0300

    Практически готовая курсовая на проверку.

--- telegram-bot/src/main/resources/liquibase/scripts/user.sql
+++ telegram-bot/src/main/resources/liquibase/scripts/user.sql
@@ -1,6 +1,11 @@
 -- liquibase formatted sql
 
 -- changeset elena_zh:1
-CREATE TABLE users (
-    id SERIAL,
-    email TEXT
+CREATE TABLE notification_tasks (
+    id          UUID DEFAULT gen_random_uuid(),
+    chat_id     BIGINT NOT NULL,
+    notification    TEXT,
+    alarm_date      TIMESTAMP,
+
+    CONSTRAINT  notification_tasks_pk   PRIMARY KEY (id)
+);
